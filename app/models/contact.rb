1c6c522d627f55399fdbbadfec977ac8a3907221
Jim Rash <jim@jimrash.com>
2015-08-27 19:00:58 +0000

    Added validations and create action

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,3 +1,5 @@
 class Contact < ActiveRecord::Base
-    
+    # from http://guides.rubyonrails.org/active_record_validations.html
+    validates :name, presence: true
+    validates :email, presence: true
 end
